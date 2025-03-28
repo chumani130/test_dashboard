8da6d14bbf37313f27e97f30b27b73558aadfd0f
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-03-28 16:01:10 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,9 +10,9 @@ export const routes: Routes = [
     {
         path: 'login', component: LoginComponent,
     },
-    // {
-    //     path:'', component: NavbarComponent
-    // },
+    { 
+        path: '**', redirectTo: 'login' 
+    },
     {
         path: 'dashboard',component: DashboardComponent
     }
