b3f0075ebab1792b0be57d6cb255b8a520c25e84
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-04-14 11:10:17 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { NavbarComponent } from './pages/navbar/navbar.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { TablesComponent } from './pages/tables/tables.component';
+import { ChartsComponent } from './pages/charts/charts.component';
+import { FogotPasswordComponent } from './pages/fogot-password/fogot-password.component';
 
 export const routes: Routes = [
     {
@@ -10,10 +14,27 @@ export const routes: Routes = [
     {
         path: 'login', component: LoginComponent,
     },
+    {
+        path: 'error', component: ErrorPageComponent
+    },
     // { 
     //     path: '**', redirectTo: 'login' 
     // },
     {
         path: 'dashboard',component: DashboardComponent
-    }
+    },
+    {
+        path: 'forgot-password',component: FogotPasswordComponent
+    },
+    
+    {
+        path: 'register', component: RegisterComponent
+    },
+    {
+        path: 'tables', component: TablesComponent
+    },
+    {
+        path: 'charts',component: ChartsComponent
+    },
+    
 ];
