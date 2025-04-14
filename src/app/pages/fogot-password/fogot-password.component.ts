52555b39e46c7082c6caabd7bda59d107565694d
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-04-14 11:10:51 +0200

    integrating bootstrap into angular
    integrating bootstrap into angular

---
+++ src/app/pages/fogot-password/fogot-password.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fogot-password',
+  standalone: true,
+  imports: [],
+  templateUrl: './fogot-password.component.html',
+  styleUrl: './fogot-password.component.css'
+})
+export class FogotPasswordComponent {
+
+}
